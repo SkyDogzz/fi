b7102d14ce50e28aabfd68fea9257bb5ef1ce4aa
Thomas Stephan <tstephan@bess-f2r6s5.clusters.42paris.fr>
2025-04-04 19:24:41 +0200

    how dumb am i ? (are data race in the room with us ?)

--- philo.c
+++ philo.c
@@ -6,22 +6,16 @@
 /*   By: skydogzz <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/07 00:38:18 by skydogzz          #+#    #+#             */
-/*   Updated: 2025/04/04 18:49:25 by tstephan         ###   ########.fr       */
+/*   Updated: 2025/04/04 19:23:06 by tstephan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int	main(int argc, char **argv)
+static bool	init_philo_threads(t_data data)
 {
-	t_data		data;
 	int			i;
-	pthread_t	monitor;
 
-	if (!parse_args(argc, argv, &data))
-		return (1);
-	if (!init_data(&data))
-		return (1);
 	i = 0;
 	while (i < data.number_of_philosophers)
 	{
@@ -29,22 +23,45 @@ int	main(int argc, char **argv)
 				(void *)&(data.philos[i])) != 0)
 		{
 			printf("Error creating thread for philosopher %d\n", i + 1);
-			return (1);
+			return (false);
 		}
 		i++;
 	}
-	if (pthread_create(&monitor, NULL, monitor_routine, (void *)&data) != 0)
-	{
-		printf("Error creating monitor thread\n");
-		return (1);
-	}
-	pthread_join(monitor, NULL);
 	i = 0;
 	while (i < data.number_of_philosophers)
 	{
 		pthread_join(data.philos[i].thread, NULL);
 		i++;
 	}
+	return (true);
+}
+
+static bool	init_monitor_thread(t_data data)
+{
+	pthread_t	monitor;
+
+	if (pthread_create(&monitor, NULL, monitor_routine, (void *)&data) != 0)
+	{
+		printf("Error creating monitor thread\n");
+		return (false);
+	}
+	pthread_join(monitor, NULL);
+	return (true);
+}
+
+int	main(int argc, char **argv)
+{
+	t_data		data;
+	int			i;
+
+	if (!parse_args(argc, argv, &data))
+		return (1);
+	if (!init_data(&data))
+		return (1);
+	if (!init_philo_threads(data))
+		return (1);
+	if (!init_monitor_thread(data))
+		return (1);
 	i = 0;
 	while (i < data.number_of_philosophers)
 	{
