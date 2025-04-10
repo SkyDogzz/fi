3969a97de92bb6aaf0ada27af4a34692ef40b85a
Thomas Stephan <tstephan@paul-f4Ar8s2.clusters.42paris.fr>
2025-04-10 18:42:21 +0200

    test

--- philo.c
+++ philo.c
@@ -6,7 +6,7 @@
 /*   By: skydogzz <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/07 00:38:18 by skydogzz          #+#    #+#             */
-/*   Updated: 2025/04/07 20:53:57 by tstephan         ###   ########.fr       */
+/*   Updated: 2025/04/10 18:40:47 by tstephan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -87,3 +87,39 @@ int	main(int argc, char **argv)
 	free(data.philos);
 	return (0);
 }
+
+/*#!/bin/bash*/
+/*# Ce script lance x fois la commande ./philo 5 800 200 200 7 */
+/*# et stocke chaque sortie dans le dossier "log" :*/
+/*# - Un fichier logX pour chaque exécution*/
+/*# - Un fichier "log" cumulant toutes les sorties*/
+/*# 🚀💾*/
+/**/
+/*# Vérifier que l'utilisateur a fourni le nombre d'exécutions*/
+/*if [ "$#" -ne 1 ]; then*/
+/*    echo "Utilisation : $0 nombre_d_execution 🔢"*/
+/*    exit 1*/
+/*fi*/
+/**/
+/*nombre_exec=$1*/
+/**/
+/*# Création du dossier "log" s'il n'existe pas 🚧*/
+/*if [ ! -d "log" ]; then*/
+/*    mkdir log*/
+/*fi*/
+/**/
+/*# Réinitialiser le fichier global et afficher un message de démarrage 🎬*/
+/*echo "=== Début des exécutions === 🏁" > log/log*/
+/**/
+/*# Boucle de 1 à nombre_exec pour lancer les commandes*/
+/*for (( i=1; i<=nombre_exec; i++ )); do*/
+/*    echo "Exécution numéro $i 🚀" | tee -a log/log*/
+/*    # Exécution de la commande et stockage dans "log/logX"*/
+/*    ./philo 5 800 200 200 7 > "log/log$i"*/
+/*    # Ajout de la sortie du fichier logX dans le log global*/
+/*    cat "log/log$i" | tee -a log/log*/
+/*    echo "-------------------------- 🔻" | tee -a log/log*/
+/*done*/
+/**/
+/*echo "=== Exécutions terminées 🎉 ===" | tee -a log/log*/
+
